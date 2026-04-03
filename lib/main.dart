0eeeb4d7ba7f99733e5fa821f664655f9254288c
Exauce Bodi <exaucebodi4@gmail.com>
2026-04-03 17:24:02 +0200

    Injection de CarteService et NfcService

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+import 'package:app_mobile/business/services/carte/carteNetworkService.dart';
+import 'package:app_mobile/business/services/nfc/nfcService.dart';
 import 'package:app_mobile/business/services/user/userLocalService.dart';
 import 'package:app_mobile/business/services/user/userNetworkService.dart';
+import 'package:app_mobile/framework/carte/carteNetworkServiceImpl.dart';
+import 'package:app_mobile/framework/nfc/nfcServiceImpl.dart';
 import 'package:app_mobile/framework/user/userLocalServiceImpl.dart';
 import 'package:app_mobile/framework/user/userNetworkServiceImpl.dart';
 import 'package:app_mobile/framework/utils/http/remoteHttpUtils.dart';
@@ -12,22 +16,27 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:get_it/get_it.dart';
 import 'package:get_storage/get_storage.dart';
 
-
 GetIt getIt = GetIt.instance;
 
-void configureImplementations(){
-
+void configureImplementations() {
   var localManager = GetStorageImpl();
   var httpUtils = RemoteHttpUtils();
   var baseUrl = dotenv.env['API_BASE_URL'] ?? '';
 
   getIt.registerLazySingleton<NavigationUtils>(() => NavigationUtils());
-  getIt.registerLazySingleton<UserLocalService>(() => UserLocalServiceImpl(box: localManager));
-  getIt.registerLazySingleton<UserNetworkService>(() => UserNetworkServiceImpl(baseUrl: baseUrl, httpUtils: httpUtils));
+  getIt.registerLazySingleton<UserLocalService>(
+    () => UserLocalServiceImpl(box: localManager),
+  );
+  getIt.registerLazySingleton<UserNetworkService>(
+    () => UserNetworkServiceImpl(baseUrl: baseUrl, httpUtils: httpUtils),
+  );
+  getIt.registerLazySingleton<CarteNetworkService>(
+    () => CarteNetworkServiceImpl(baseUrl: baseUrl, httpUtils: httpUtils),
+  );
+  getIt.registerLazySingleton<NfcService>(() => NfcServiceImpl());
 }
 
-void main() async{
-
+void main() async {
   //Initialisation de dotenv pour charger les variables d'environnement
   await dotenv.load(fileName: ".env");
 
@@ -38,4 +47,3 @@ void main() async{
 
   runApp(ProviderScope(child: MyApplication()));
 }
-
