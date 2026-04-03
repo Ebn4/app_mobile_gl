8c9b0f30d27e20bd9c19bbd09102ba5a8cb65ec2
Exauce Bodi <exaucebodi4@gmail.com>
2026-04-03 17:23:07 +0200

    Modif de la variable isLoading dans loginState

--- lib/pages/auth/login/loginState.dart
+++ lib/pages/auth/login/loginState.dart
@@ -1,11 +1,11 @@
 import 'package:app_mobile/business/models/user/user.dart';
 
 class LoginState {
-  final bool isLoading;
-  final String error;
-  final User user;
+  final bool? isLoading;
+  final String? error;
+  final User? user;
 
-  LoginState({required this.isLoading, required this.error, required this.user});
+  LoginState({this.isLoading, this.error, this.user});
   
   LoginState copyWith({bool? isLoading, String? error, User? user}) {
     return LoginState(
